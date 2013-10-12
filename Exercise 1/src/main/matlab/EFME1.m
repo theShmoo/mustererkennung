b9133be0b7d11a8c3c354587b7e95f2f73dfdaab
David Pfahler <david@pfahler.at>
2013-10-12 14:17:47 +0200

    [API] First File
    The first matlab source file is now on the repository and the project goes ino development! From now on use the __dev__ branch!
    Branch for new features!

---
+++ Exercise 1/src/main/matlab/EFME1.m
@@ -0,0 +1,18 @@
+%% EFME 2013 LU Exercise 1
+% Authors:
+% * David Pfahler
+% * Matthias Gusenbauer
+% * Matthias Vigele
+
+function [ featureVector ] = EFME1( inputImage )
+%This function returns the feature vector of an bw input image from the
+%MPEG7 CE Shape-1 Part B Database
+%INPUT:
+%   inputImage ... a bw image of the MPEG7 CE Shape-1 Part B Database 
+%OUTPUT:
+%   featureVector ... a vector with the values of the extracted Features
+
+
+
+end
+
