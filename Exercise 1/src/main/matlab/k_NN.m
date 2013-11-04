491a5cf68a36eda638169ac8d308f66ae295d878
David Pfahler <david@pfahler.at>
2013-11-04 19:23:27 +0100

    [FEATURE] k-NN in main
    * Ready for testing and optimization!
    * Graphs would be nice

--- Exercise 1/src/main/matlab/k_NN.m
+++ Exercise 1/src/main/matlab/k_NN.m
@@ -1,12 +1,12 @@
 %% kNN
 % Author _Matthias Vigele_
 %
-% This function returns the classes of a set of feature vectors by using 
+% This function returns the classes of a set of feature vectors by using
 % the k-Nearest Neighbour Classifier
 %% INPUT
 %   featureSet ... a matrix with values of extracted Features, values
 %   describing the same image are in the same row
-%   trainingSet ... a matrix countaining values of 
+%   trainingSet ... a matrix countaining values of
 %   extracted Features of the same kind as used in the featureSet
 %   trainingLabels ... a cell vector containing the names of the classes
 %   that correspond to the images inside the training set.
@@ -22,38 +22,37 @@
 % vec = k_NN(featureSet, trainingSet,trainingLabels,1);
 
 function [classNames]= k_NN(featureSet,trainingSet,trainingLabels,k)
-    classNames = cell(length(featureSet),1);
-    for i = 1:size(featureSet,1)
-        feature=featureSet(i,:);
-        distances= zeros([length(feature) 1]);
-        trainingLabelsC=trainingLabels;
-%%Compute Euclidean distance between feature and each member of the training
-%set       
+classNames = cell(size(featureSet,1),1);
+for i = 1:size(featureSet,1)
+    feature=featureSet(i,:);
+    distances= zeros([length(feature) 1]);
+    trainingLabelsC=trainingLabels;
+    %%Compute Euclidean distance between feature and each member of the training
+    %set
     for j = 1:size(trainingSet,1)
         training=trainingSet(j,:);
-        distance=(feature-training).^2;
-        distances(j)=sqrt(sum(distance));
+        distances(j)=norm(feature-training);
     end
-
     
-%%Select the k Nearest Neighbours
-classes=unique(trainingLabelsC);
-classesCount= zeros([length(classes) 1]);
-mcClass=0;
+    
+    %%Select the k Nearest Neighbours
+    classes=unique(trainingLabelsC);
+    classesCount= zeros([length(classes) 1]);
+    mcClass=0;
     for j = 1:k
         nn=find(distances==min(distances));
         ci=find(not(cellfun('isempty', strfind(classes,trainingLabelsC{nn}))));
-        classesCount(ci)=classesCount(ci)+1;      
-  %feature gets labeled with the class name, that is most common among the 
-  %k Nearest Neighbours   
-  if (classesCount(ci)>mcClass)
-  classNames{i}=trainingLabelsC{nn};
-  mcClass=classesCount(ci);
-  end  
-        distances(nn)=[]; 
-        trainingLabelsC(nn)=[];      
+        classesCount(ci)=classesCount(ci)+1;
+        %feature gets labeled with the class name, that is most common among the
+        %k Nearest Neighbours
+        if (classesCount(ci)>mcClass)
+            classNames{i}=trainingLabelsC{nn};
+            mcClass=classesCount(ci);
+        end
+        distances(nn)=[];
+        trainingLabelsC(nn)=[];
     end
     
     
-    end
+end
 end
--- Exercise 1/src/main/matlab/main.m
+++ Exercise 1/src/main/matlab/main.m
@@ -7,9 +7,10 @@
 
 function main()
 %% Get Input Images
-classes = {'bat';'apple';'beetle';'bell';'chicken'};
+classes = {'Heart';'octopus';'rat';'device1';'watch'};
 classSize = 20;
 [images,classnames] = getInput(classes);
+imageCount = length(images);
 % Classnames to numbers
 % This is for numbers
 % [ ~,~,class ] = unique(classnames,'stable'); geht bei matlab 2011/b ned...
@@ -33,13 +34,14 @@ classSize = 20;
 
 propertiesSelection = [4, 8, 21]; 
 featureNames = selectFeatureNames(propertiesSelection);
-features = zeros(length(images),length(featureNames));
-%features = cell(length(images), length(featureNames));
+featureCount = length(featureNames);
+features = zeros(imageCount,featureCount );
+%features = cell(imageCount, featureCount );
 
 disp('Starting Feature Detection...');
 fprintf(' %s',featureNames{:});
 fprintf('\n');
-for i = 1:length(images)
+for i = 1:imageCount
     features(i,:) = getFeatures(images{i},featureNames);
 end
 disp('Feature Detection finished');
@@ -47,9 +49,9 @@ disp('Feature Detection finished');
 figure('name','The Dependency Of The Features');
 
 plot = 1;
-for i = 1 : length(featureNames) 
-    for j = i+1 : length(featureNames)
-            subplot(length(featureNames),1,plot);
+for i = 1 : featureCount  
+    for j = i+1 : featureCount 
+            subplot(featureCount ,1,plot);
             scatter(features(:,i),features(:,j),40,class,'filled');
             %scatter([features{:,i}],[features{:,j}],40,class,'filled');
             xlabel(featureNames{i});
@@ -61,8 +63,8 @@ end
 %% BoxPlot Features
 figure('name','The Features');
 
-for i = 1 : length(featureNames)
-    subplot(length(featureNames),1,i);
+for i = 1 : featureCount 
+    subplot(featureCount ,1,i);
     boxplot(features(:,i),classnames);
     title(featureNames{i});
 end
@@ -71,8 +73,8 @@ end
 figure('name','Features as PDF')
 cc=hsv(length(classes));
 
-for i = 1 : length(featureNames)
-    subplot(length(featureNames),1,i);
+for i = 1 : featureCount 
+    subplot(featureCount ,1,i);
     
     for j = 1 : length(classes)
         f = features(1+(j-1)*classSize:j*classSize,i);
@@ -83,5 +85,13 @@ for i = 1 : length(featureNames)
     end
     title(featureNames{i});
 end
-
+%% k-NN
+result = cell(imageCount);
+for k = 1:imageCount-1
+    fprintf('k = %d\n',k);
+    for i = 1:imageCount
+        bufferCell = k_NN(features(i,:),[features(1:i-1,:);features(1+i:end,:)],classnames,k);
+        result{i,k} = bufferCell{1};
+    end
+end
 end
