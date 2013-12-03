56c97538a602a2929fe62214dd71ae1e14f9dbeb
Matthias Gusenbauer <matthias.gusenbauer@gmx.net>
2013-12-03 23:26:15 +0100

    finished mahalanobis

--- Exercise 2/src/calculateMahalanobis.m
+++ Exercise 2/src/calculateMahalanobis.m
@@ -3,14 +3,18 @@ function [ dist1, dist2, dist3 ] = calculateMahalanobis( class1, class2, class3,
 %   Detailed explanation goes here
 
     %calculate the mean vectors
-    meanC1 = mean(class1);
-    meanC2 = mean(class2);
-    meanC3 = mean(class3);
+    meanC1 = mean(class1(:,2:end));
+    meanC2 = mean(class2(:,2:end));
+    meanC3 = mean(class3(:,2:end));
     
     %calculate the covariance matrices
-    covMat1 = cov(class1(1, 2:end));
-    covMat2 = cov(class2(1, 2:end));
-    covMat3 = cov(class3(1, 2:end));
+    covMat1 = cov(class1(:, 2:end));
+    covMat2 = cov(class2(:, 2:end));
+    covMat3 = cov(class3(:, 2:end));
+   
+    dist1 = ((sample - meanC1) * inv(covMat1) * transpose((sample - meanC1)));
+    dist2 = ((sample - meanC2) * inv(covMat2) * transpose((sample - meanC2)));
+    dist3 = ((sample - meanC3) * inv(covMat3) * transpose((sample - meanC3)));
     
     % either mahal(X,Y) x - sample; y - reference
     % or d(I) = (Y(I,:)-mu)*inv(SIGMA)*(Y(I,:)-mu)
