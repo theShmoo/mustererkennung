371194d822a6fe4a9918c821c5ff76fedf2f1b27
Javaki <m.vigele@inode.at>
2013-12-15 21:56:11 +0100

    Plot of the Discriminate Function

--- Exercise 2/src/main.m
+++ Exercise 2/src/main.m
@@ -114,5 +114,52 @@ xlabel('# of Testrun');
 ylabel('Successrate in percent');
 title('Barplot of Mahalanobis results');
 
+%Plot of the Discriminate Function
+figure('name','Discriminate Function of set A = [1 2 2 3; 2 1 3 1] and set B = [5 6 4;2 3 4]');
+S1 = [1;2;2;3;5;6;4];
+S2 = [2;1;3;1;2;3;4];
+group = [1 1 1 1 2 2 2];
+
+h1 = gscatter(S1,S2,group,'rb','v^',[],'off');
+
+set(h1,'LineWidth',2)
+legend('Class A','Class B',...
+       'Location','NW')
+
+[X,Y] = meshgrid(linspace(0,10),linspace(0,10));
+X = X(:); Y = Y(:);
+[C,err,P,logp,coeff] = classify([X Y],[S1 S2],...
+                                group,'mahalanobis');
+
+hold on;
+gscatter(X,Y,C,'rb','.',1,'off');
+K = coeff(1,2).const;
+L = coeff(1,2).linear; 
+Q = coeff(1,2).quadratic;
+% Function to compute K + L*v + v'*Q*v for multiple vectors
+% v=[x;y]. Accepts x and y as scalars or column vectors.
+f = @(x,y) K + [x y]*L + sum(([x y]*Q) .* [x y], 2);
+
+h2 = ezplot(f,[0 10 0 10]);
+set(h2,'Color','m','LineWidth',2)
+
+%%Plot handwritten result(yellow)
+f2 = @(x,y) -1/4*x^2-25/6*x-11/3*y+591/24-1/2*log(0.5)+1/2*log(3/4);
+
+h3 = ezplot(f2,[0 10 0 10]);
+set(h3,'Color','y','LineWidth',2)
+
+%%Plot mean values m1=(2 7/4) m2=(5 3)
+h4 = gscatter([2;5],[7/4;3],[1 2],'cg','v^',[],'off');
+line([2;5],[7/4;3],'Color','r','LineWidth',1);
+legend('Class A','Class B','Class A Area','Class B Area',...
+'Discriminate Function (Matlab)','Discriminate Function (Handwritten)',...
+'Mean value Class A','Mean value Class B','Mean value connection line',...
+       'Location','NE')
+
+axis([0 10 0 10])
+xlabel('Value 1')
+ylabel('Value 2')
+title('{\bf Classification of set A = [1 2 2 3; 2 1 3 1] and set B = [5 6 4;2 3 4]}')
 end
 
