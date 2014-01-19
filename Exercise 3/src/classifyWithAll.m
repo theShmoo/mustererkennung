34a4036cef5fc8b6f01755bf2b1b043255ac3cb8
Matthias Gusenbauer <matthias.gusenbauer@gmx.net>
2014-01-19 16:12:07 +0100

    [FIXED] nn in cassify all

--- Exercise 3/src/classifyWithAll.m
+++ Exercise 3/src/classifyWithAll.m
@@ -95,8 +95,8 @@ function [resultMahalanobis2C, resultMahalanobis6C, resultkNN2C, resultkNN6C, re
     % then runs classification with NN
     addpath('nn');
     
-    [error2C, resultNN2C, percent2CError] = classifyWithNN(test2C, prepareTargets(testClasses2C, 2), training2C, prepareTargets(trainingClasses2C, 2));
-    [error6C, resultNN6C, percent6CError] = classifyWithNN(test6C, prepareTargets(testClasses6C, 6), training6C, prepareTargets(trainingClasses6C, 6));
+    resultNN2C = classifyWithNN(test2C, prepareTargets(testClasses2C, 2), training2C, prepareTargets(trainingClasses2C, 2));
+    resultNN6C = classifyWithNN(test6C, prepareTargets(testClasses6C, 6), training6C, prepareTargets(trainingClasses6C, 6));
     
 %     fprintf('The neural network has %3.2f%% percent errors for the 2 class problem.\n', percent2CError);
 %     fprintf('The neural network has %3.2f%% percent errors for the 6 class problem.\n', percent6CError);
